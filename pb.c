5c1c9ddedfb51f6d39942fcb67364717239c7952
katty87 <esenenkova@gmail.com>
2020-09-10 21:49:52 +0300

    Read all pb values

--- pb.c
+++ pb.c
@@ -15,6 +15,14 @@ typedef struct pbheader_s {
 } pbheader_t;
 #define PBHEADER_INIT {MAGIC, 0, 0}
 
+const char *DEVICE_KEY = "device";
+const char *LAT_KEY = "lat";
+const char *LON_KEY = "lon";
+const char *APPS_KEY = "apps";
+
+const char *TYPE_KEY = "type";
+const char *ID_KEY = "id";
+
 
 // https://github.com/protobuf-c/protobuf-c/wiki/Examples
 void example() {
@@ -55,6 +63,68 @@ void example() {
     free(buf);
 }
 
+
+// Parse deviceapps item of input list
+// Return DeviceApps msg
+static DeviceApps parse_item(PyObject *dict_item) {
+    DeviceApps msg = DEVICE_APPS__INIT;
+    DeviceApps__Device device = DEVICE_APPS__DEVICE__INIT;
+    msg.device = &device;
+
+    PyObject *device_info = PyDict_GetItemString(dict_item, DEVICE_KEY);
+    if ( (device_info != NULL) && PyDict_Check(device_info) ) {
+        PyObject *type = PyDict_GetItemString(device_info, TYPE_KEY);
+        if ( (type != NULL) && PyMapping_Check(type) ) {
+            char *device_type = PyUnicode_AsUTF8(type);
+            device.has_type = 1;
+            device.type.data = (uint8_t*)device_type;
+            device.type.len = strlen(device_type);
+        }
+
+        PyObject *id = PyDict_GetItemString(device_info, ID_KEY);
+        if ( (id != NULL) && PyMapping_Check(id)) {
+            char *device_id = PyUnicode_AsUTF8(id);
+            device.has_id = 1;
+            device.id.data = (uint8_t*)device_id;
+            device.id.len = strlen(device_id);
+        }
+    }
+
+    PyObject *lat = PyDict_GetItemString(dict_item, LAT_KEY);
+    if ( (lat != NULL) && ( PyFloat_Check(lat) || PyLong_Check(lat) ) ) {
+        msg.has_lat = 1;
+        msg.lat = PyFloat_AsDouble(lat);
+    }
+
+    PyObject *lon = PyDict_GetItemString(dict_item, LON_KEY);
+    if ( (lon != NULL) && ( PyFloat_Check(lon) || PyLong_Check(lon) ) ) {
+        msg.has_lon = 1;
+        msg.lon = PyFloat_AsDouble(lon);
+    }
+
+    PyObject *apps = PyDict_GetItemString(dict_item, APPS_KEY);
+    if ( PyList_Check(apps) ) {
+        int size = PyList_Size(apps);
+
+        msg.n_apps = size;
+        msg.apps = malloc(sizeof(uint32_t) * msg.n_apps);
+
+        int i = 0;
+        for (i = 0; i < size; i++)
+        {
+            PyObject *app = PyList_GetItem(apps, i);
+            if ( PyLong_Check(app) ) {
+                msg.apps[i] = PyLong_AsUnsignedLong(app);
+            } else {
+                msg.apps[i] = 0;
+            }
+        }
+    }
+
+    return msg;
+}
+
+
 // Read iterator of Python dicts
 // Pack them to DeviceApps protobuf and write to file with appropriate header
 // Return number of written bytes as Python integer
@@ -68,23 +138,26 @@ static PyObject* py_deviceapps_xwrite_pb(PyObject* self, PyObject* args) {
     printf("********************************************\n");
     printf("Write to: %s\n", path);
 
-    PyObject *key, *value;
-    Py_ssize_t pos = 0;
     char *key_string = 0;
+
+    if ( !PyList_Check(list) ) {
+        printf("Input parameter is not a list\n");
+        Py_RETURN_NONE;
+    }
 
     int size = PyList_Size(list);
     printf("size=%d\n", size);
 
-
-    for (int i = 0; i < size; i++)
+    int i = 0;
+    for (i = 0; i < size; i++)
     {
-        PyObject *dict = PyList_GetItem(list, i);
+        printf("ind=%d\n", i);
 
-        while (PyDict_Next(dict, &pos, &key, &value)) {
-            printf("get dict item\n");
-            key_string = PyUnicode_AsUTF8(key);
-            printf("%s\n", key_string);
-            printf("\n");
+        PyObject *dict = PyList_GetItem(list, i);
+        if ( PyDict_Check(dict) ) {
+            DeviceApps msg = parse_item(dict);
+            printf("type=%s, id=%s, lat=%.10f, lon=%.10f\n", msg.device->type.data, msg.device->id.data, msg.lat, msg.lon);
+            free(msg.apps);
         }
     }
 
@@ -93,6 +166,7 @@ static PyObject* py_deviceapps_xwrite_pb(PyObject* self, PyObject* args) {
     int bytes_written = -1;
     return PyLong_FromLong(bytes_written);
 }
+
 
 // Unpack only messages with type == DEVICE_APPS_TYPE
 // Return iterator of Python dicts
